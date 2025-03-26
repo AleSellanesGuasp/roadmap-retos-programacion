0ac1de5a25945a69288b36476fcd1409b0f722d6
AleSellanesGuasp <150531963+AleSellanesGuasp@users.noreply.github.com>
2025-03-26 11:00:26 -0400

    AleSellanesGuasp.py
    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AleSellanesGuasp.py
@@ -0,0 +1,25 @@
+# https://www.python.org/
+
+"""
+La url de mi lenguaje de interes es:
+https://www.python.org/
+"""
+
+'''
+La url de mi lenguaje de interes es:
+https://www.python.org/
+'''
+
+mi_lenguaje = "Quiero aprender a programar"
+mi_lenguaje = "Quiero aprender a programar con Python"
+
+MY_CONSTANT = "Roadmap retos de programacion"
+
+var_num_int = 1
+var_num_float = 1.2
+var_bool_1 = True
+var_bool_0 = False
+var_string = "Cadena de Texto"
+
+print("¡Hola, Python!")
+
